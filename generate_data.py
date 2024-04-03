f42da5ee3ba65cea7cadde3ed3abdcd57162c84a
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2024-04-03 15:18:46 +0300

    generate many data files

---
+++ generate_data.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import os
+
+
+try:
+    os.makedirs("data")
+except:
+    pass
+
+for year in range(2016,2024):
+   for month in range(1,13):
+     for day in range(31):
+      print(year, month)
+      try:
+          os.makedirs(f"data/activity_{year}_{month}")
+      except:
+          pass
+      data = []
+      for hour in range(24):
+          data.append({
+             "hour": hour,
+             "activity_level": np.random.randint(1,5)
+          })
+      data = pd.DataFrame(data)
+      data.to_csv(f"data/activity_{year}_{month}/activity_{year}_{month}_{day}.csv")
+
+
+
