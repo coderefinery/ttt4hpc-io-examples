95c600cb4e80afc13cd7000f5252d81332042523
Jarno Rantaharju <jarno.rantaharju@aalto.fi>
2024-04-08 11:18:45 +0300

    Simplify also random access

--- data_formats_1/read_archive_random.py
+++ data_formats_1/read_archive_random.py
@@ -1,25 +1,25 @@
 import io
 import tarfile
 import pandas as pd
-import numpy as np
 import time
 
-# iterate over files in data.zip matching data/*/*.csv
-start_time = time.time()
 
+# Read in random order. Extract the list of file names and
+# randomize.
+start_time = time.time()
 csv = "index,hour,activity_level\n"
-with tarfile.open("data.tar") as tar:
+with tarfile.open("data.tar", "r") as tar:
     filenames = [member.name for member in tar if member.name.endswith(".csv")]
-    # randomize order
-    filenames = np.random.permutation(filenames)
     for member in filenames:
         f = tar.extractfile(member)
-        f = io.TextIOWrapper(f)
-        csv += "\n".join(f.readlines()[1:])
+        lines = f.read().decode("utf-8").split("\n")
+        csv += "\n".join(lines[1:])
+
 
 data = pd.read_csv(io.StringIO(csv))
 mean = data["activity_level"].mean()
 
+
 end_time = time.time()
 print(f"Time taken: {end_time - start_time} seconds")
-print(mean)
+print(f"Mean: {mean}")
