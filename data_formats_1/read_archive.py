58a1764fbf760a6b146fa78beca47ccad2fc9070
Jarno Rantaharju <jarno.rantaharju@aalto.fi>
2024-04-08 10:50:18 +0300

    Make streaming mode clearer

--- data_formats_1/read_archive.py
+++ data_formats_1/read_archive.py
@@ -5,11 +5,19 @@ import tarfile
 import pandas as pd
 
 
+streaming_mode = True
+
+if streaming_mode:
+    mode = "r|"
+else
+    mode = "r"
+
+
 # Read sequantially. The "r|" sets tar reading mode to
 # streaming
 start_time = time.time()
 csv = "index,hour,activity_level\n"
-with tarfile.open("data.tar", "r|") as tar:
+with tarfile.open("data.tar", mode) as tar:
     for member in tar:
         if member.name.endswith(".csv"):
             f = tar.extractfile(member)
