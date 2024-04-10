c0e1e7d661141852e92fab6bc683b6bcac315a13
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2024-04-10 15:46:38 +0300

    Use the mnist data in reading example. The predownloaded imagenet is not generally available

---
+++ webdataset/read.py
@@ -0,0 +1,23 @@
+import webdataset as wds
+import glob
+
+
+# List all shards
+urls = glob.glob("*.tar")
+
+# Create a dataset. Set it up to shuffle in batches of 100 images.
+dataset = (
+    wds.WebDataset(urls)
+    .shuffle(100)
+    .decode("rgb")
+    .to_tuple("input.pyd", "output.pyd", "__key__")
+)
+
+
+# At this point we have not actually read the files.
+# Thea are read when we actually iterate over them
+for i, (image, output, key) in enumerate(dataset):
+    print(image, output, key)
+    
+    if i == 3:
+        break
