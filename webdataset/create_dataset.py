610f68e55c1d4d3c133fadf7ea9ef1aaa06013a1
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2024-04-22 15:31:53 +0300

    More than 1 shard and create in data folder

--- webdataset/create_dataset.py
+++ webdataset/create_dataset.py
@@ -5,7 +5,7 @@ import sys
 
 dataset = torchvision.datasets.MNIST(root="/tmp", download=True)
 
-tarred_dataset = wds.ShardWriter("mnist_%d.tar")
+tarred_dataset = wds.ShardWriter("data/mnist_%d.tar", maxcount=10000)
 for index, (input, output) in enumerate(tqdm(dataset)):
     tarred_dataset.write({
         "__key__": "sample%06d" % index,
